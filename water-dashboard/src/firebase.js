8ede2af148f11f444dc99049d3cd9a93a5c59391
Sam_Gunarathne <22ug3-0503@sltc.ac.lk>
2026-05-01 14:51:07 +0530

    water-dashboard/src/firebase.js dashboard fix

--- water-dashboard/src/firebase.js
+++ water-dashboard/src/firebase.js
@@ -9,6 +9,9 @@ const firebaseConfig = {
   authDomain: "aqualytics-649ed.firebaseapp.com",
   databaseURL: "https://aqualytics-649ed-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "aqualytics-649ed",
+  storageBucket: "aqualytics-649ed.firebasestorage.app",
+  messagingSenderId: "823456739352",
+  appId: "1:823456739352:web:4c379b7bda19716f7d936d"
 };
 
 const app = initializeApp(firebaseConfig);
