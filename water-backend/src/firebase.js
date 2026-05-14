ca3edd094508db8fe5fcb8f43b126092dafe7416
Sam_Gunarathne <22ug3-0503@sltc.ac.lk>
2026-05-15 01:34:38 +0530

    firebase.js databaseURL update

--- water-backend/src/firebase.js
+++ water-backend/src/firebase.js
@@ -8,7 +8,7 @@ import { getAnalytics } from "firebase/analytics";
 const firebaseConfig = {
   apiKey: "AIzaSyBn2zkxkdKM3fvzhjfqOa1IGJkWA8Fi13U",
   authDomain: "aqualytics-649ed.firebaseapp.com",
-  databaseURL: "https://aqualytics-649ed-default-rtdb.asia-southeast1.firebasedatabase.app",
+  databaseURL: "https://aqualytics-649ed-default-rtdb.asia-southeast1.firebasedatabase.app/",
   projectId: "aqualytics-649ed",
   storageBucket: "aqualytics-649ed.firebasestorage.app",
 };
