7acccefc1f6e2232f6a5d985fde0cf0c8dcfbfea
Asha Nethmini <your_noreply_email>
2026-05-16 17:03:11 +0530

    new

--- water-backend/src/firebase.js
+++ water-backend/src/firebase.js
@@ -2,19 +2,17 @@
 
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
-import { getAnalytics } from "firebase/analytics";
+//import { getAnalytics } from "firebase/analytics";
 
-// 🔐 Your Firebase config (from Firebase Console)
+// Your Firebase config (from Firebase Console)
 const firebaseConfig = {
   apiKey: "AIzaSyBn2zkxkdKM3fvzhjfqOa1IGJkWA8Fi13U",
   authDomain: "aqualytics-649ed.firebaseapp.com",
-  databaseURL: "https://aqualytics-649ed-default-rtdb.asia-southeast1.firebasedatabase.app",
+  databaseURL: "https://aqualytics-649ed-default-rtdb.asia-southeast1.firebasedatabase.app/",
   projectId: "aqualytics-649ed",
   storageBucket: "aqualytics-649ed.firebasestorage.app",
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+//const analytics = getAnalytics(app);
 export const db = getDatabase(app);
-
-
