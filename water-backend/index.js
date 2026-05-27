637fe8a284b717e546e9b9e6d848be663143d88a
Sam_Gunarathne <22ug3-0503@sltc.ac.lk>
2026-05-27 16:32:17 +0530

    mqtt.connect == bevywise

--- water-backend/index.js
+++ water-backend/index.js
@@ -43,7 +43,7 @@ if (!fs.existsSync(BUFFER_FILE)) {
 // ==========================================
 
 const client = mqtt.connect(
-  'mqtt://broker.hivemq.com:1883',
+  'mqtt://public-mqtt-broker.bevywise.com:1883',
   {
 
     //username: process.env.MQTT_USERNAME,
