81ee18022cc86affeb181ba3a0d90215793a4766
shashini-dissanayaka <shashiniudara344@gmail.com>
2026-07-08 01:19:18 +0530

    update app js

--- water-dashboard/src/App.js
+++ water-dashboard/src/App.js
@@ -1,15 +1,76 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+
+import { auth } from "./firebase";
+
+import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 
-function App() {
+
+function App(){
+
+  const [user,setUser] = useState(null);
+  const [loading,setLoading] = useState(true);
+
+
+  useEffect(()=>{
+
+    const unsubscribe = onAuthStateChanged(auth,(currentUser)=>{
+
+      setUser(currentUser);
+      setLoading(false);
+
+    });
+
+
+    return ()=>unsubscribe();
+
+  },[]);
+
+
+
+  if(loading){
+    return <h2>Loading...</h2>;
+  }
+
+
+
   return (
-    <div>
-      <h1 style={{ textAlign: "center" }}>
-        Water Quality Monitoring Dashboard
-      </h1>
-      <Dashboard />
-    </div>
+
+    <>
+    
+    {
+      user ?
+
+      (
+        <>
+        
+              {user ? (
+        <Dashboard />
+      ) : (
+        <Login setUser={setUser} />
+      )}
+
+
+        <Dashboard/>
+
+        </>
+      )
+
+      :
+
+      (
+        <Login/>
+      )
+
+    }
+
+
+    </>
+
   );
+
 }
+
 
 export default App;
